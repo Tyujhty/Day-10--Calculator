53354dc4e5d50d3b3b5236a7fe84103e754c6c3f
Tyujhty <fannybaquey@gmail.com>
2023-12-29 18:24:08 +0000

    feat: add logo. Create while loop .

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Functions of calculator
+from art import logo
 
 #Add
 def add(n1, n2):
@@ -22,21 +22,37 @@ operations = {
     "*": multiply,
     "/": divide
 }
+def calculate(num1, num2, operation_symbole):
+    return operations[operation_symbole](num1, num2)
 
-num1 = int(input("What is the first number?: "))
-num2 = int(input("What is the seconde number?: "))
+print(logo)
+
+num1 = float(input("What is the first number?: "))
+num2 = float(input("What is the seconde number?: "))
 
 for operator in operations:
     print(operator)
 
-
 answer = 0
 operation_symbole = input("Pick an operation from the line above: ")
 
-def calculate(num1, num2, operation_symbole):
-    answer = operations[operation_symbole](num1, num2)
-    return answer
-
-calculate(num1, num2, operation_symbole)
+answer = calculate(num1, num2, operation_symbole)
 
 print(f"{num1} {operation_symbole} {num2} = {answer}")
+
+continue_calculate = True
+
+while continue_calculate == True:
+    
+    continue_calculate = input(f"Type 'y' to continue with {answer}, or type 'n' to quit.: ").lower()
+    
+    if continue_calculate == 'y':
+        num1 = answer
+        operation_symbole = input("Pick a symbole: ")
+        num2 = float(input("Pick a number: "))
+        
+        answer = calculate(num1, num2, operation_symbole)
+        print(f"{num1} {operation_symbole} {num2} = {answer}")
+        continue_calculate = True
+    else:
+        print("Goodbye")
